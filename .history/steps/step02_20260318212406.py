506fdb190f4c3d2578a3b23039b1d3c6b19fb3b2
liuxiaoxiong <liuxiaoxiong@neolix.cn>
2026-03-26 19:44:19 +0800

    remove .history directory

--- .history/steps/step02_20260318212406.py
+++
@@ -1,9 +0,0 @@
-class Function:
-    def __call__(self, input):
-        x = input.data
-        y = self.forward(x)
-        output = Variable(y)
-        reutrn output
-
-    def forward(self, x):
-        raise NotImplementedError()
